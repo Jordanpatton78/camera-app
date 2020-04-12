7a301b33a819dc7c2df45021d97dcf2cdd2aee35
Jordanpatton78 <44303813+Jordanpatton78@users.noreply.github.com>
2020-04-12 13:52:16 -0700

    Update app.js

--- app.js
+++ app.js
@@ -27,8 +27,7 @@ cameraTrigger.onclick = function() {
     window.location.href=cameraOutput.src;
     cameraOutput.classList.add("taken");
     Tesseract.recognize(ctx).then(function(result){
-        var resultText = result.text;
-        window.print(resultText);
+        document.getElementById("ocr-results").innerText=result.text;
     })
 }
                                
